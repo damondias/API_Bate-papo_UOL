2e9c8d4e088d09d93278079c75020792bd3dd7e4
Damon <damondias@gmail.com>
2022-01-31 11:10:55 -0400

    Criando POST da rota /participants

--- src/index.js
+++ src/index.js
@@ -17,6 +17,53 @@ mongoClient.connect(() => {
   db = mongoClient.db("batepapouol");
 });
 
+app.post("/participants", async (req, res) => {
+    try {
+      const uol = mongoClient.db("batepapouol");
+      const participants = uol.collection("participants");
+      const messages = uol.collection("messages");
+  
+      const online = await participants.find({}).toArray();
+  
+      let participantsList = online.map((el) => el.name);
+  
+      const username = req.body;
+      const userSchema = joi.object().keys({
+        name: joi
+          .string()
+          .invalid(...participantsList)
+          .required(),
+      });
+  
+      const validation = userSchema.validate(username, { abortEarly: true });
+  
+      if (validation.error) {
+        if (validation.error.details[0].type === "any.invalid") {
+          res.status(409).send("Usuário já existente");
+        } else {
+          res.status(422).send("Insira um nome válido");
+        }
+        return;
+      }
+  
+      username.lastStatus = Date.now();
+      await participants.insertOne(username);
+  
+      let StatusMsg = {
+        from: username.name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs(username.lastStatus).format("HH:mm:ss"),
+      };
+      await messages.insertOne(StatusMsg);
+  
+      res.sendStatus(201);
+    } catch {
+      res.sendStatus(500);
+    }
+  });
+
 app.listen(5000, () => {
     console.log("Rodando API Bate Papo Uol em http://localhost:5000");
   });
